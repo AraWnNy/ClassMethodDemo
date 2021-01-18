62a4a8c7c01aecc4890cf4ecad6ec37c88ae7c53
Fikret Gözütok <fikret_gztk@hotmail.com>
2021-01-18 22:27:28 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void addCustomer(Customer customer)
+        {
+            Console.WriteLine(String.Format("Müşteri Kaydı Eklendi {0} {1}", customer.name, customer.surname));
+        }
+
+        public void deleteCustomer(Customer customer)
+        {
+            Console.WriteLine(String.Format("Müşteri Kaydı Silindi {0} {1}", customer.name, customer.surname));
+        }
+
+        public void getCustomerData(Customer customer)
+        {
+            Console.WriteLine(String.Format("Müşterinin;\nAdı = {0}\nSoyadı =  {1}\nTC Kimlik No = {2}\nAdres = {3}",customer.name, customer.surname, customer.id, customer.address));
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager manager = new CustomerManager();
+            Customer c1 = new Customer {
+                name = "Fikret",
+                surname = "Gözütok",
+                id = "12323222",
+                address = "Kütahya"};
+
+            Customer c2 = new Customer
+            {
+                name = "İsmail",
+                surname = "Demir",
+                id = "111122233323",
+                address = "Tavşanlı"
+            };
+
+            manager.deleteCustomer(c1);
+            manager.addCustomer(c2);
+            manager.getCustomerData(c2);
+
+
+
+            Console.Read();
+
+        }
+    }
+}
